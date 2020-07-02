64de43e067858d22ed93b6643195361052ad85f9
Luca Bettiol <luca.bettiol.92@gmail.com>
2020-07-02 15:56:33 +0200

    Initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
+/*
+ * @Author: Luca Bettiol
+ * @Date:   2020-07-02T14:11:00+02:00
+ * @Email:  luca.bettiol.92@gmail.com
+ * @Filename: scripts.js
+ * @Last modified by:   Luca Bettiol
+ * @Last modified time: 2020-07-02T14:33:59+02:00
+ * @Copyright: Copyright (c) 2020 by Bettiol Luca. All Rights Reserved.
+ */
+
 $(document).ready(function(){
   $(window).scroll(function(){
-    if(this.scrollY > 20) 
+    if(this.scrollY > 20)
       $(".navbar").addClass("sticky");
     else
       $(".navbar").removeClass("sticky");
